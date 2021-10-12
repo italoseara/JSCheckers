a2576da73ff59e7f7e8370460cd52ce37b0e453d
Italo Seara <italo.sseara@gmail.com>
2021-10-12 15:50:04 -0300

    Game UI

---
+++ scripts/logic.js
@@ -0,0 +1,227 @@
+// LOGIC
+
+function movement(e, element) {
+
+    if (gameTurn != element.classList[1]) return;
+
+    document.querySelectorAll(".prediction, .clone").forEach(element => {
+        element.remove();
+    })
+
+    if (document.querySelector(".moving"))
+        document.querySelector(".moving").classList.remove("moving");
+
+    // MOVING
+    let clone = element.cloneNode();
+    clone.innerHTML = element.innerHTML;
+    clone.classList.add("clone");
+    clone.style.position = "absolute";
+        
+    document.body.appendChild(clone);
+    element.classList.add("moving");
+    
+    moveClone(e)
+    element.style.display = "none";
+    // ======
+
+    const coords = getCoordinates(element);
+    
+    drawPredictions(element, coords)
+
+    document.querySelectorAll(".prediction").forEach(element_prediction => {
+        element_prediction.addEventListener("mouseup", () => {
+    
+            let moving_piece = document.querySelector(".moving");
+            let clone = document.querySelector(".clone");
+            
+            if (clone) {
+                clone.remove()
+                moving_piece.style.display = "block";
+            }
+            
+            const coords = getCoordinates(element_prediction);
+            let promotion = false;
+
+            if (
+                !element.classList.contains("dame") &&
+                element.classList[1] == "white" &&
+                coords[0] == 0
+            ) {
+        
+                element.classList.add("dame");
+                element.innerHTML = `<i class="fas fa-crown" style="font-size:4vh; position: relative; top: 50%; left: 50%; transform: translate(-50%, -50%); color: var(--white-piece-outer);"></i>`;
+
+                promote.play();
+                promotion = true;
+        
+            } else if (
+                !element.classList.contains("dame") &&
+                element.classList[1] == "black" &&
+                coords[0] == 7
+            ) {
+        
+                element.classList.add("dame");
+                element.innerHTML = `<i class="fas fa-crown" style="font-size:4vh; position: relative; top: 50%; left: 50%; transform: translate(-50%, -50%); color: var(--black-piece-outer);"></i>`;
+
+                promote.play();
+                promotion = true;
+            } 
+
+            element_prediction.parentNode.appendChild(moving_piece);
+            
+            document.querySelectorAll(".prediction").forEach(e => {
+                e.remove();
+            })
+
+            if (element_prediction.classList[1]) {
+                let x = element_prediction.classList[1][0];
+                let y = element_prediction.classList[1][2];
+
+                table[x][y].firstChild.remove();
+
+                if (drawPredictions(element, getCoordinates(element))) {
+                    gameTurn = gameTurn == "white" ? "black" : "white";
+                } else {
+                    document.querySelectorAll(".prediction").forEach(e => {
+                        e.remove();
+                    })
+                }
+                if (!promotion)
+                    capture.play();
+            } else {
+                if (!promotion)
+                    move.play();
+            }
+            
+            gameTurn = gameTurn == "white" ? "black" : "white";
+            
+            document.querySelector(`.active`).classList.toggle("active");
+            document.querySelector(`#player-${gameTurn}`).classList.toggle("active");
+        })
+    })
+}
+
+
+function drawPredictions(element, coords) {
+
+    let canEat = false;
+    let prediction_;
+
+    if (element.classList.contains("black") || element.classList.contains("dame")) {
+
+        try {
+
+        if (table[coords[0]+1][coords[1]+1])
+            
+            prediction_ = prediction.cloneNode();
+
+            if (!table[coords[0]+1][coords[1]+1].firstChild)
+                table[coords[0]+1][coords[1]+1]
+                    .appendChild(prediction_.cloneNode());
+            
+            else {
+                if (table[coords[0]+1][coords[1]+1].firstChild
+                    .classList[1] != element.classList[1] &&
+                    table[coords[0]+2][coords[1]+2] &&
+                    !table[coords[0]+2][coords[1]+2].firstChild
+                ) {
+                    prediction_.classList.add(`${[coords[0]+1]}-${[coords[1]+1]}`);
+
+                    table[coords[0]+2][coords[1]+2]
+                        .appendChild(prediction_.cloneNode());
+
+                    canEat = true;
+                }
+            }
+        
+        } catch (error) { }
+
+        try {
+
+        if (table[coords[0]+1][coords[1]-1])
+
+            prediction_ = prediction.cloneNode();
+
+            if (!table[coords[0]+1][coords[1]-1].firstChild)
+                table[coords[0]+1][coords[1]-1]
+                    .appendChild(prediction_.cloneNode());
+            
+            else {
+                if (table[coords[0]+1][coords[1]-1].firstChild
+                    .classList[1] != element.classList[1] &&
+                    table[coords[0]+2][coords[1]-2] && 
+                    !table[coords[0]+2][coords[1]-2].firstChild
+                ) {
+                    prediction_.classList.add(`${[coords[0]+1]}-${[coords[1]-1]}`);
+
+                    table[coords[0]+2][coords[1]-2]
+                        .appendChild(prediction_.cloneNode());
+
+                    canEat = true;
+                }
+            }
+
+        } catch (error) { }
+
+    } 
+    
+    if (element.classList.contains("white") || element.classList.contains("dame")) {
+
+        try {
+
+        if (table[coords[0]-1][coords[1]+1])
+
+            prediction_ = prediction.cloneNode();
+
+            if (!table[coords[0]-1][coords[1]+1].firstChild)
+                table[coords[0]-1][coords[1]+1]
+                    .appendChild(prediction_.cloneNode());
+            
+            else {
+                if (table[coords[0]-1][coords[1]+1].firstChild
+                    .classList[1] != element.classList[1] &&
+                    table[coords[0]-2][coords[1]+2] &&
+                    !table[coords[0]-2][coords[1]+2].firstChild
+                ) {
+                    prediction_.classList.add(`${[coords[0]-1]}-${[coords[1]+1]}`);
+
+                    table[coords[0]-2][coords[1]+2]
+                        .appendChild(prediction_.cloneNode());
+
+                    canEat = true;
+                }
+            }
+
+        } catch (error) { }
+
+        try {
+
+        if (table[coords[0]-1][coords[1]-1])
+        
+            prediction_ = prediction.cloneNode();
+
+            if (!table[coords[0]-1][coords[1]-1].firstChild)
+                table[coords[0]-1][coords[1]-1]
+                    .appendChild(prediction_.cloneNode());
+            
+            else {
+                if (table[coords[0]-1][coords[1]-1].firstChild
+                    .classList[1] != element.classList[1] &&
+                    table[coords[0]-2][coords[1]-2] && 
+                    !table[coords[0]-2][coords[1]-2].firstChild
+                ) {
+                    prediction_.classList.add(`${[coords[0]-1]}-${[coords[1]-1]}`);
+
+                    table[coords[0]-2][coords[1]-2]
+                        .appendChild(prediction_.cloneNode());
+
+                    canEat = true;
+                }
+            }
+        
+        } catch (error) { }
+            
+    }
+    
+    return canEat;
+}
---
+++ scripts/start.js
@@ -0,0 +1,146 @@
+let rows, moving, clone, prediction;
+
+let xRel = 0;
+let yRel = 0;
+let table = [];
+let holding = false;
+
+let gameTurn = "white";
+
+const move = new Audio('audios/move-self.webm');
+const capture = new Audio('audios/capture.webm');
+const promote = new Audio('audios/promote.webm');
+const gameStart = new Audio('audios/game-start.webm');
+
+// DRAW TABLE
+let black = true;
+
+for (let i = 0; i < 8; i++) {
+
+    black = !black
+
+    let div = document.createElement("div");
+    div.setAttribute("class", `row`)
+
+    for (let i = 0; i < 8; i++) {
+        let square = document.createElement("div");
+
+        if (black) {
+            square.setAttribute("class", `square black`);
+        } else {
+            square.setAttribute("class", `square white`);
+        }
+
+        div.appendChild(square);
+        black = !black
+    }
+
+    document.querySelector(".table").appendChild(div);
+}
+
+function restartGame() {
+    gameStart.play();
+    
+    // DRAW PIECES
+    gameTurn = "white";
+    
+    if (document.querySelector(`.active`))
+        document.querySelector(`.active`).classList.toggle("active");
+        
+    document.querySelector(`#player-${gameTurn}`).classList.add("active");
+
+    document.querySelectorAll(".piece, .prediction").forEach(todelete => {
+        todelete.remove();
+    })
+
+    let piece = document.createElement("div");
+    rows = document.querySelectorAll(".row")
+    black = true;
+
+    rows.forEach(element => {
+        if ([0, 1, 2].includes([...rows].indexOf(element))) {
+            piece.setAttribute("class", "piece black");
+
+            element.querySelectorAll(".square").forEach(element2 => {
+                if (element2.classList.contains("black")) {
+                    element2.appendChild(piece.cloneNode());
+                }
+            })
+        }
+
+        if ([5, 6, 7].includes([...rows].indexOf(element))) {
+            piece.setAttribute("class", "piece white");
+
+            element.querySelectorAll(".square").forEach(element2 => {
+                if (element2.classList.contains("black")) {
+                    element2.appendChild(piece.cloneNode());
+                }
+            })
+        }
+    });
+
+    // ANIMATION
+    rows.forEach(element => {
+        table.push(element.querySelectorAll(".square"));
+    })
+
+    prediction = document.createElement("div");
+    prediction.setAttribute("class", "prediction");
+
+    document.querySelectorAll(".piece").forEach(element => {
+        element.addEventListener("mousedown", (e) => {
+            movement(e, element);
+        })
+    });
+
+    document.querySelector(".table").addEventListener("mousemove", (e) => { if (holding) moveClone(e); })
+    document.querySelector(".table").addEventListener("mousedown", () => { holding = true; })
+    document.querySelector(".table").addEventListener("mouseup", () => {
+        holding = false;
+        xRel = 0;
+        yRel = 0;
+
+        clone = document.querySelector(".clone");
+        moving = document.querySelector(".moving");
+
+        if (moving) moving.style.display = "block";
+        if (clone) clone.remove();
+    })
+
+}
+
+function getCoordinates(element) {
+    for (let i = 0; i < table.length; i++) {
+        let i2 = -1;
+
+        for (let j = 0; j < [...table[i]].length; j++) {
+            if ([...table[i]][j].contains(element))
+                i2 = j;
+        }
+        if (i2 !== -1) return [i, i2];
+    }
+    return undefined;
+}
+
+
+function moveClone(e) {
+
+    let clone = document.querySelector(".clone");
+    let moving = document.querySelector(".moving");
+
+    if (moving) {
+        let moving_rect = moving.getBoundingClientRect()
+
+        if (yRel == 0) {
+            xRel = e.x - moving_rect.left;
+            yRel = e.y - moving_rect.top;
+        }
+    }
+
+    if (clone) {
+        clone.style.left = (e.x - xRel) + 'px';
+        clone.style.top = (e.y - yRel) + 'px';
+    }
+
+}
+
